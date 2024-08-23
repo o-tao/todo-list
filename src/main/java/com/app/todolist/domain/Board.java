2381b13abd9739d13ac2d5b47354c5e7417bdb27
o-tao <dhtkdals369@gmail.com>
2024-08-24 02:27:03 +0900

    공통 컬럼 BaseEntity에 추가, auditing 사용

--- src/main/java/com/app/todolist/domain/Board.java
+++ src/main/java/com/app/todolist/domain/Board.java
@@ -1,25 +1,13 @@
 package com.app.todolist.domain;
 
-import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import lombok.Getter;
 
 @Entity
-@Table(name = "board")
-public class Board {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Getter
+public class Board extends BaseEntity {
 
     private String title;
     private String content;
 
-    @Column(nullable = false, updatable = false, insertable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime createdAt;
-
-    @Column(nullable = false, insertable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime updatedAt;
 }
