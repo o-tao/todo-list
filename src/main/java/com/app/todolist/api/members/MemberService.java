8b64137328f12a0aba2ffd470e6cb828d0d93e81
o-tao <dhtkdals369@gmail.com>
2024-09-18 16:36:22 +0900

    중복회원 생성 예외처리 구현

--- src/main/java/com/app/todolist/api/members/MemberService.java
+++ src/main/java/com/app/todolist/api/members/MemberService.java
@@ -12,7 +12,14 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member create(Member member) {
+        validateMember(member.getEmail());
         return memberRepository.save(member);
+    }
+
+    public void validateMember(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 
 }
