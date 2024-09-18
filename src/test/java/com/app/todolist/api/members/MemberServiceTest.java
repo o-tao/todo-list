6e7fa99e7d948428dd5bc5b2b9207adc8c169c6c
o-tao <dhtkdals369@gmail.com>
2024-09-18 19:49:41 +0900

    중복회원 생성 테스트코드 @Transactional 어노테이션 제거

--- src/test/java/com/app/todolist/api/members/MemberServiceTest.java
+++ src/test/java/com/app/todolist/api/members/MemberServiceTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@Transactional
 class MemberServiceTest {
 
     @Autowired
