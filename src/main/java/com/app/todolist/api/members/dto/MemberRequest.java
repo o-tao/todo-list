aaba7a63e26abef5300fabbaa406fd24fb097f4b
o-tao <dhtkdals369@gmail.com>
2024-09-27 13:13:44 +0900

    이메일 형식 패턴 추가

--- src/main/java/com/app/todolist/api/members/dto/MemberRequest.java
+++ src/main/java/com/app/todolist/api/members/dto/MemberRequest.java
@@ -2,6 +2,7 @@ package com.app.todolist.api.members.dto;
 
 import com.app.todolist.domain.members.Member;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MemberRequest {
 
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일을 입력하세요.")
     private String email;
 
