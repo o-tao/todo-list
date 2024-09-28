15a02c777dd8295d8830a462242d274cbcb18469
o-tao <dhtkdals369@gmail.com>
2024-09-29 03:05:23 +0900

    Todo 생성 커스텀 예외처리 추가

--- src/main/java/com/app/todolist/api/todos/TodoService.java
+++ src/main/java/com/app/todolist/api/todos/TodoService.java
@@ -4,6 +4,8 @@ import com.app.todolist.domain.members.Member;
 import com.app.todolist.domain.members.MemberRepository;
 import com.app.todolist.domain.todos.Todo;
 import com.app.todolist.domain.todos.TodoRepository;
+import com.app.todolist.exception.ErrorCode;
+import com.app.todolist.exception.TodoApplicationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +20,8 @@ public class TodoService {
     private final MemberRepository memberRepository;
 
     public Todo createTodo(Long memberId, String title, String content) {
-        Member member = memberRepository.findById(memberId).orElseThrow();
+        Member member = memberRepository.findById(memberId).orElseThrow(()
+                -> new TodoApplicationException(ErrorCode.NOT_EXISTED_EMAIL));
         return todoRepository.save(Todo.create(member, title, content));
     }
 }
