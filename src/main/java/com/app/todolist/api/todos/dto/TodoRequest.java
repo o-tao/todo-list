3afc73637a15d3f09ac4581f97892b234bbb2160
o-tao <dhtkdals369@gmail.com>
2024-10-01 23:47:34 +0900

    TodoRequest 최소 입력값, 최대 입력값 설정

--- src/main/java/com/app/todolist/api/todos/dto/TodoRequest.java
+++ src/main/java/com/app/todolist/api/todos/dto/TodoRequest.java
@@ -1,6 +1,7 @@
 package com.app.todolist.api.todos.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,8 +13,9 @@ public class TodoRequest {
 
     private Long memberId;
 
+    @Size(min = 2, max = 30, message = "제목은 2자리 이상, 30자리 이하 입력이 가능합니다.")
     @NotBlank(message = "제목을 입력하세요.")
     private String title;
-    
+
     private String content;
 }
