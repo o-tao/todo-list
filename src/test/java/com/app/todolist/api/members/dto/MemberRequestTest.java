232e0cbf87df75d0d4efccab90409eaddc9eb107
o-tao <dhtkdals369@gmail.com>
2024-10-03 00:33:38 +0900

    테스트코드 수정 ControllerTest -> RequestTest

---
+++ src/test/java/com/app/todolist/api/members/dto/MemberRequestTest.java
@@ -0,0 +1,127 @@
+package com.app.todolist.api.members.dto;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRequestTest {
+
+    private Validator validator = null;
+    private MemberRequest memberRequest;
+
+    @BeforeEach
+    public void setup() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+        memberRequest = new MemberRequest();
+    }
+
+    @Test
+    @DisplayName("아이디를 입력하지 않을 경우 예외가 발생한다.")
+    public void idValidTest() {
+        // given
+        memberRequest.setEmail(null);
+        memberRequest.setPassword("1234");
+
+        // when
+        Set<ConstraintViolation<MemberRequest>> violations = validator.validate(memberRequest);
+
+        // then
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains("이메일을 입력하세요.")
+                .hasSize(1);
+    }
+
+    @Test
+    @DisplayName("아이디를 입력할 경우 예외가 발생하지 않는다.")
+    public void idSuccessTest() {
+        // given
+        memberRequest.setEmail("tao@exemple.com");
+        memberRequest.setPassword("1234");
+
+        // when
+        Set<ConstraintViolation<MemberRequest>> violations = validator.validate(memberRequest);
+
+        // then
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains()
+                .hasSize(0);
+    }
+
+    @Test
+    @DisplayName("비밀번호를 입력하지 않을 경우 예외가 발생한다.")
+    public void passwordValidTest() {
+        // given
+        memberRequest.setEmail("tao@exemple.com");
+        memberRequest.setPassword(null);
+
+        // when
+        Set<ConstraintViolation<MemberRequest>> violations = validator.validate(memberRequest);
+
+        // then
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains("비밀번호를 입력하세요.")
+                .hasSize(1);
+    }
+
+    @Test
+    @DisplayName("비밀번호를 입력할 경우 예외가 발생하지 않는다.")
+    public void passwordSuccessTest() {
+        // given
+        memberRequest.setEmail("tao@exemple.com");
+        memberRequest.setPassword("1234");
+
+        // when
+        Set<ConstraintViolation<MemberRequest>> violations = validator.validate(memberRequest);
+
+        // then
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains()
+                .hasSize(0);
+    }
+
+    @Test
+    @DisplayName("이메일 형식이 아닐 경우 예외가 발생한다.")
+    public void emailPatternTest() {
+        // given
+        memberRequest.setEmail("tao123");
+        memberRequest.setPassword("1234");
+
+        // when
+        Set<ConstraintViolation<MemberRequest>> violations = validator.validate(memberRequest);
+
+        // then
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains("이메일 형식이 올바르지 않습니다.")
+                .hasSize(1);
+    }
+
+    @Test
+    @DisplayName("이메일 형식일 경우 예외가 발생하지 않는다.")
+    public void emailPatternSuccessTest() {
+        // given
+        memberRequest.setEmail("tao123@exemple.com");
+        memberRequest.setPassword("1234");
+
+        // when
+        Set<ConstraintViolation<MemberRequest>> violations = validator.validate(memberRequest);
+
+        // then
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains()
+                .hasSize(0);
+    }
+
+}
