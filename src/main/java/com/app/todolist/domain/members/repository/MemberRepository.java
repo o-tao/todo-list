c5cca1bc19e9a4192c17bc77f23adc6d95cd3ba8
o-tao <dhtkdals369@gmail.com>
2024-10-06 02:32:08 +0900

    Repository 디렉토리 구조 변경

--- src/main/java/com/app/todolist/domain/members/repository/MemberRepository.java
+++ src/main/java/com/app/todolist/domain/members/repository/MemberRepository.java
@@ -1,5 +1,6 @@
-package com.app.todolist.domain.members;
+package com.app.todolist.domain.members.repository;
 
+import com.app.todolist.domain.members.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
--- src/main/java/com/app/todolist/domain/todos/repository/TodoRepository.java
+++ src/main/java/com/app/todolist/domain/todos/repository/TodoRepository.java
@@ -1,7 +1,7 @@
-package com.app.todolist.domain.todos;
+package com.app.todolist.domain.todos.repository;
 
+import com.app.todolist.domain.todos.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-
 }
