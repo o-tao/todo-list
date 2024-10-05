d7b2c06d9721dfb3fdfadbe05e3993f579a78a2d
o-tao <dhtkdals369@gmail.com>
2024-10-06 03:47:08 +0900

    QueryDslConfig, CustomRepository 생성ˆ

---
+++ src/main/java/com/app/todolist/domain/todos/repository/TodoCustomRepository.java
@@ -0,0 +1,38 @@
+package com.app.todolist.domain.todos.repository;
+
+import com.app.todolist.domain.todos.Todo;
+import com.app.todolist.domain.todos.TodoStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.app.todolist.domain.todos.QTodo.todo;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Todo> findByTitleContains(Long memberId, String title, TodoStatus status) {
+        return jpaQueryFactory
+                .selectFrom(todo)
+                .where(
+                        todo.member.id.eq(memberId)
+                                .and(titleEq(title))
+                                .and(statusEq(status))
+                )
+                .fetch();
+    }
+
+    private BooleanExpression titleEq(String title) {
+        return title != null ? todo.title.contains(title) : null;
+    }
+
+    private BooleanExpression statusEq(TodoStatus status) {
+        return status != null ? todo.status.eq(status) : null;
+    }
+}
