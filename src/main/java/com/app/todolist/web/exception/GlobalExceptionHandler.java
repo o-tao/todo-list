f72a043ef3071ba2a61a66c75dcb7ef406681f09
o-tao <dhtkdals369@gmail.com>
2024-10-24 00:40:55 +0900

    JSON parse에러 ExceptionHandler처리

--- src/main/java/com/app/todolist/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/todolist/web/exception/GlobalExceptionHandler.java
@@ -1,15 +1,14 @@
 package com.app.todolist.web.exception;
 
 import com.app.todolist.web.exception.dto.TodoExceptionResponse;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
 import java.util.Objects;
 
 @RestControllerAdvice
@@ -24,12 +23,15 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<TodoExceptionResponse> responseMethodArgumentNotValidException(BindException exception) {
-        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-        String message = fieldErrors.stream().map(FieldError::getField).toList().toString();
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<TodoExceptionResponse> responseMethodArgumentNotValidException(HttpMessageNotReadableException exception) {
+        InvalidFormatException invalidFormatException = (InvalidFormatException) exception.getCause();
+        String fieldName = invalidFormatException.getPath().getFirst().getFieldName();
+        String targetType = invalidFormatException.getTargetType().getSimpleName();
+
+        String errorMessage = String.format(ErrorCode.INVALID_JSON_INPUT.getMessage(), fieldName, targetType);
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        String errorMessage = message + " 필드 값이 잘못되었습니다.";
+        
         return new ResponseEntity<>(
                 new TodoExceptionResponse(httpStatus, Objects.requireNonNull(errorMessage)), httpStatus);
     }
