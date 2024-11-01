51136c9825dbd127465cbc27d81070be0cc89115
o-tao <dhtkdals369@gmail.com>
2024-11-01 18:22:13 +0900

    TodoServiceTest 중복 생성 로직 메서드 처리

--- src/test/java/com/app/todolist/api/todos/service/TodoServiceTest.java
+++ src/test/java/com/app/todolist/api/todos/service/TodoServiceTest.java
@@ -39,13 +39,22 @@ class TodoServiceTest {
         memberRepository.deleteAllInBatch();
     }
 
+    private Member createMember(String email, String password) {
+        Member member = Member.create(email, password);
+        return memberRepository.save(member);
+    }
+
+    private Todo createTodo(Member member, String title, String content) {
+        Todo todo = Todo.create(member, title, content);
+        return todoRepository.save(todo);
+    }
+
     @Test
     @Transactional
     @DisplayName("존재하는 회원이 Todo생성 시 Todo가 저장된다.")
     public void createTodoTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = createMember("tao@exemple.com", "1234");
         String title = "tao title";
         String content = "tao content";
         Long memberId = savedMember.getId();
@@ -56,8 +65,8 @@ class TodoServiceTest {
         // then
         Todo findTodo = todoRepository.findAll().stream().findFirst().orElseThrow();
         Member findMember = findTodo.getMember();
-        assertThat(member.getEmail()).isEqualTo(findMember.getEmail());
-        assertThat(member.getPassword()).isEqualTo(findMember.getPassword());
+        assertThat(savedMember.getEmail()).isEqualTo(findMember.getEmail());
+        assertThat(savedMember.getPassword()).isEqualTo(findMember.getPassword());
         assertThat(findTodo.getTitle()).isEqualTo(createTodo.getTitle());
         assertThat(findTodo.getContent()).isEqualTo(createTodo.getContent());
     }
@@ -82,13 +91,12 @@ class TodoServiceTest {
     @DisplayName("회원 본인이 생성한 Todo를 검색하면 검색한 회원의 Todo가 조회된다.")
     public void todoSearchTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = createMember("tao@exemple.com", "1234");
 
-        Todo todo = todoService.createTodo(savedMember.getId(), "todo title", "hello");
+        Todo todo = createTodo(savedMember, "todo title", "hello");
 
         TodosWithOptions todosWithOptions = new TodosWithOptions(
-                member.getId(), "todo title", TodoStatus.TODO, 1, 10
+                savedMember.getId(), "todo title", TodoStatus.TODO, 1, 10
         );
 
         // when
@@ -107,17 +115,16 @@ class TodoServiceTest {
     @DisplayName("title에 'tao'를 포함한 Todo 검색결과를 가져온다.")
     public void searchTodosByTitleContainsTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = createMember("tao@exemple.com", "1234");
 
-        todoService.createTodo(savedMember.getId(), "test todo title", "hello");
-        todoService.createTodo(savedMember.getId(), "hello tao! title", "world");
-        todoService.createTodo(savedMember.getId(), "hello world tao! title", "hello");
-        todoService.createTodo(savedMember.getId(), "hello tao! test title", "test");
-        todoService.createTodo(savedMember.getId(), "hello world title", "test");
+        createTodo(savedMember, "test todo title", "hello");
+        createTodo(savedMember, "hello tao! title", "world");
+        createTodo(savedMember, "hello world tao! title", "hello");
+        createTodo(savedMember, "hello tao! test title", "test");
+        createTodo(savedMember, "hello world title", "test");
 
         TodosWithOptions todosWithOptions = new TodosWithOptions(
-                member.getId(), "tao", TodoStatus.TODO, 1, 10
+                savedMember.getId(), "tao", TodoStatus.TODO, 1, 10
         );
 
         // when
@@ -132,17 +139,16 @@ class TodoServiceTest {
     @DisplayName("title에 'tao'를 포함한 Todo가 없을 경우 빈 리스트를 반환한다.")
     public void searchTodosByTitleContains_isEmpty() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = createMember("tao@exemple.com", "1234");
 
-        todoService.createTodo(savedMember.getId(), "test todo title", "hello");
-        todoService.createTodo(savedMember.getId(), "hello! title", "world");
-        todoService.createTodo(savedMember.getId(), "hello world! title", "hello");
-        todoService.createTodo(savedMember.getId(), "hello! test title", "test");
-        todoService.createTodo(savedMember.getId(), "hello world title", "test");
+        createTodo(savedMember, "test todo title", "hello");
+        createTodo(savedMember, "hello! title", "world");
+        createTodo(savedMember, "hello world! title", "hello");
+        createTodo(savedMember, "hello! test title", "test");
+        createTodo(savedMember, "hello world title", "test");
 
         TodosWithOptions todosWithOptions = new TodosWithOptions(
-                member.getId(), "tao", TodoStatus.TODO, 1, 10
+                savedMember.getId(), "tao", TodoStatus.TODO, 1, 10
         );
 
         // when
@@ -173,10 +179,8 @@ class TodoServiceTest {
     @DisplayName("존재하는 todo의 title만 변경 될 경우 title만 수정된다.")
     public void todoTitleUpdateTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
-        Todo todo = Todo.create(savedMember, "todo-list", "hello");
-        Todo existingTodo = todoRepository.save(todo);
+        Member savedMember = createMember("tao@exemple.com", "1234");
+        Todo existingTodo = createTodo(savedMember, "todo-list", "hello");
 
         Long todoId = existingTodo.getId();
         TodoUpdateInfo updateInfo = new TodoUpdateInfo("title update", "hello");
@@ -187,17 +191,15 @@ class TodoServiceTest {
         // then
         Todo updatedTodo = todoRepository.findById(todoId).orElseThrow();
         assertEquals(updateInfo.getTitle(), updatedTodo.getTitle());
-        assertEquals(todo.getContent(), updatedTodo.getContent());
+        assertEquals(existingTodo.getContent(), updatedTodo.getContent());
     }
 
     @Test
     @DisplayName("존재하는 todo의 content만 변경 될 경우 content만 수정된다.")
     public void todoContentUpdateTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
-        Todo todo = Todo.create(savedMember, "todo-list", "hello");
-        Todo existingTodo = todoRepository.save(todo);
+        Member savedMember = createMember("tao@exemple.com", "1234");
+        Todo existingTodo = createTodo(savedMember, "todo-list", "hello");
 
         Long todoId = existingTodo.getId();
         TodoUpdateInfo updateInfo = new TodoUpdateInfo("todo-list", "content update");
@@ -207,7 +209,7 @@ class TodoServiceTest {
 
         // then
         Todo updatedTodo = todoRepository.findById(todoId).orElseThrow();
-        assertEquals(todo.getTitle(), updatedTodo.getTitle());
+        assertEquals(existingTodo.getTitle(), updatedTodo.getTitle());
         assertEquals(updateInfo.getContent(), updatedTodo.getContent());
     }
 
@@ -215,10 +217,8 @@ class TodoServiceTest {
     @DisplayName("존재하는 todo의 title과 content가 모두 변경될 경우 모두 수정된다.")
     public void todoTitleAndContentUpdateTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
-        Todo todo = Todo.create(savedMember, "todo-list", "hello");
-        Todo existingTodo = todoRepository.save(todo);
+        Member savedMember = createMember("tao@exemple.com", "1234");
+        Todo existingTodo = createTodo(savedMember, "todo-list", "hello");
 
         Long todoId = existingTodo.getId();
         TodoUpdateInfo updateInfo = new TodoUpdateInfo("title update", "content update");
@@ -236,10 +236,8 @@ class TodoServiceTest {
     @DisplayName("존재하는 todo가 수정 될 경우 todo를 수정 한 시간이 updatedAt에 저장된다.")
     public void todoUpdatedAtTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
-        Todo todo = Todo.create(savedMember, "todo-list", "hello");
-        Todo existingTodo = todoRepository.save(todo);
+        Member savedMember = createMember("tao@exemple.com", "1234");
+        Todo existingTodo = createTodo(savedMember, "todo-list", "hello");
 
         Long todoId = existingTodo.getId();
         LocalDateTime previousUpdatedAt = existingTodo.getUpdatedAt();
@@ -258,10 +256,6 @@ class TodoServiceTest {
     @DisplayName("존재하지 않는 todo 수정 시 예외가 발생한다.")
     public void todoUpdatedValidateTest() {
         // given
-        Member member = Member.create("tao@exemple.com", "1234");
-        Member savedMember = memberRepository.save(member);
-        Todo todo = Todo.create(savedMember, "todo-list", "hello");
-        todoRepository.save(todo);
         TodoUpdateInfo updateInfo = new TodoUpdateInfo("title update", "content update");
 
         // when
