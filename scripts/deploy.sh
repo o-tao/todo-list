8b560fee2981882cea4d4b02a46f4754ee59d382
o-tao <dhtkdals369@gmail.com>
2024-11-21 18:02:58 +0900

    [#56] appspec.yml, deploy.sh 경로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-{
-echo "MYSQL_URL: $MYSQL_URL"
-echo "DB_USERNAME: $DB_USERNAME"
-echo "DB_PASSWORD: $DB_PASSWORD"
-echo "REDIS_HOST: $REDIS_HOST"
-echo "REDIS_PORT: $REDIS_PORT"
-} >> /home/ubuntu/deploy.log
-
-
-DEPLOY_PATH=/home/ubuntu/todo/
-echo "배포 경로: $DEPLOY_PATH" >> /home/ubuntu/deploy.log
-
-mkdir -p $DEPLOY_PATH
-echo "디렉토리 생성 완료" >> /home/ubuntu/deploy.log
-
-BUILD_JAR=$(ls /home/ubuntu/todo/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/todo-list/build/libs/*.jar)
 JAR_NAME=$(basename "$BUILD_JAR")
 echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
+DEPLOY_PATH=/home/ubuntu/todo-list/
 cp "$BUILD_JAR" "$DEPLOY_PATH"
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
