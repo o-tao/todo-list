a1686d274008d48dfad060c5c48f45d858860946
o-tao <dhtkdals369@gmail.com>
2024-11-21 21:42:47 +0900

    [#56] 환경변수 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-TIME=$(date "+%Y-%m-%d %H:%M:%S")
-
-{
-echo "[$TIME] MYSQL_URL: $MYSQL_URL"
-echo "[$TIME] DB_USERNAME: $DB_USERNAME"
-echo "[$TIME] DB_PASSWORD: $DB_PASSWORD"
-echo "[$TIME] REDIS_HOST: $REDIS_HOST"
-echo "[$TIME] REDIS_PORT: $REDIS_PORT"
-} >> /home/ubuntu/deploy.log
-
 DEPLOY_PATH=/home/ubuntu/todo/
-echo "[$TIME] 배포 경로: $DEPLOY_PATH" >> /home/ubuntu/deploy.log
+echo "배포 경로: $DEPLOY_PATH" >> /home/ubuntu/deploy.log
 
 mkdir -p $DEPLOY_PATH
-echo "[$TIME] 디렉토리 생성 완료" >> /home/ubuntu/deploy.log
+echo "디렉토리 생성 완료" >> /home/ubuntu/deploy.log
 
 BUILD_JAR=$(ls /home/ubuntu/todo/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "[$TIME] >>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
-echo "[$TIME] >>> build 파일 복사" >> /home/ubuntu/deploy.log
+echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
 cp $BUILD_JAR $DEPLOY_PATH 2>> /home/ubuntu/deploy_err.log
 
-echo "[$TIME] >>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
 sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15 2>> /home/ubuntu/deploy_err.log
 
+echo ">>> 환경 변수 파일 다운로드" >> /home/ubuntu/deploy.log
+aws s3 cp s3://$AWS_S3_BUCKET/deploy.env $DEPLOY_PATH/deploy.env 2>> /home/ubuntu/deploy_err.log
+
+echo ">>> 환경 변수 파일 로드 및 환경 변수 설정 후 삭제" >> /home/ubuntu/deploy.log
+export $(cat $DEPLOY_PATH/deploy.env | xargs) 2>> /home/ubuntu/deploy_err.log
+rm $DEPLOY_PATH/deploy.env 2>> /home/ubuntu/deploy_err.log
+
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "[$TIME] >>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-echo "[$TIME] >>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ubuntu/deploy.log
+echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
+echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ubuntu/deploy.log
 nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log &
